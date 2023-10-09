8e4c369e1fbeed325f6bbaeef4470fdb0bbea9ef
Nkyyyy <odinakafv14423@gmail.com>
2023-10-09 08:21:11 +0100

    get a new copy without altering the original copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx <0 or idx > (len(my_list)-1):
-        return my_list
+        return (my_list)
     new_lists=[x for x in my_list]
     new_lists[idx]=element
-    return(new_lists)
+    return (new_lists)
