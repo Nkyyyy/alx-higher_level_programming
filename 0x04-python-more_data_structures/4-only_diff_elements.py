6aae22c559beea18f67f90bf33d748797dcffaa6
Nkyyyy <odinakafv14423@gmail.com>
2023-10-11 20:44:36 +0100

    different item

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    common_set = set()
+    for item in set_1
+        if item in set_2
+        common_set.sub(item)
+    return (common_set)
